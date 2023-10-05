1c11cb5f9406062b897a8c4f72238b5a684cebc0
Nadr <n.aboumoustafa@itsrizzoli.it>
2023-10-05 17:19:43 +0200

    (Scarafoni) - Prima implementazione Tab Bar

--- QuizGame/ContentView.swift
+++ QuizGame/ContentView.swift
@@ -7,6 +7,36 @@
 
 import SwiftUI
 
+struct MyTabNav: View {
+    var body: some View {
+        TabView{
+            Text("Avvia Simulazione")
+            .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "play.circle.fill")
+                    Text("Home")
+                }
+            Text("Materie")
+            .font(.system(size: 30, weight: .bold, design: .rounded))
+            .tabItem {
+            Image(systemName: "book.circle.fill")
+            Text("Materie")
+            }
+            Text("Risultati precedenti")
+            .font(.system(size: 30, weight: .bold, design: .rounded))
+            .tabItem {
+            Image(systemName: "timer.circle.fill")
+            Text("Cronologia")
+            }
+            Text("Impostazioni e profilo")
+            .font(.system(size: 30, weight: .bold, design: .rounded))
+            .tabItem {
+            Image(systemName: "gear.circle.fill")
+            Text("Impostazioni")
+            }
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
---
+++ QuizGame/HomepageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomepageView.swift
+//  QuizGame
+//
+//  Created by user243551 on 10/5/23.
+//
+
+import SwiftUI
+
+struct HomepageView: View {
+    var body: some View {
+        MyTabNav()
+    }
+}
+
+#Preview {
+    HomepageView()
+}
