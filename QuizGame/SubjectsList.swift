b02479d24910e702fd002747e7e531246a9bf635
Nadr-Abou <n.aboumoustafa@itsrizzoli.it>
2023-10-09 12:31:33 +0200

    modificato il subjectstyle

--- QuizGame/SubjectsList.swift
+++ QuizGame/SubjectsList.swift
@@ -8,31 +8,33 @@
 import SwiftUI
 
 struct SingleSubject{
-    
   var index: String
   var icon: String
   var SBJtext: String
+}
 
-  
-
+struct SubjectStyle : View{
+  var body : some View {
+    NavigationLink(destination: RegisterView()){
+      Spacer()
+      Image(systemName: value.icon)
+      Text(value.index + "-" + value.SBJtext).padding(.top,20)
+      Spacer()
+    }.frame(width: 80, height: 80)
+  }
 }
 
 struct SubjectsList: View {
     
-    let columns = [GridItem(.flexible()), GridItem(.flexible())]
-    
-    var subjects = [SingleSubject(index: "1",icon: "arrow.right",SBJtext: "UFS01"),SingleSubject(index: "2",icon: "arrow.right",SBJtext: "UFS01"),SingleSubject(index: "3",icon: "arrow.right",SBJtext: "UFS01"),SingleSubject(index: "4",icon: "arrow.right",SBJtext: "UFS01")]
+  let columns = [GridItem(.flexible()), GridItem(.flexible())]  
+  var subjects = [SingleSubject(index: "1",icon: "arrow.right",SBJtext: "UFS01"),SingleSubject(index: "2",icon: "arrow.right",SBJtext: "UFS01"),SingleSubject(index: "3",icon: "arrow.right",SBJtext: "UFS01"),SingleSubject(index: "4",icon: "arrow.right",SBJtext: "UFS01")]
+  
   var body: some View {
     NavigationStack{
       ScrollView{
         LazyVGrid(columns: columns){
             ForEach(subjects, id: \.index){
-              value in NavigationLink(destination: RegisterView()){
-                  Spacer()
-                  Image(systemName: value.icon)
-                  Text(value.index + "-" + value.SBJtext).padding(.top,20)
-                  Spacer()
-                }.frame(width: 80, height: 80)
+              value in SubjectStyle(value)
           }
         }
       }
