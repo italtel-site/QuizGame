0dc94faa33ee3df51bdc7e932c7528bad19cee97
Riccardo.Frigerio <134052922+Riky000000003@users.noreply.github.com>
2023-10-11 22:04:34 +0200

    modifica di DettagliSimulazione

--- QuizGame/DettagliSimulazioneView.swift
+++ QuizGame/DettagliSimulazioneView.swift
@@ -7,12 +7,41 @@
 
 import SwiftUI
 
+struct Intestazione: View 
+{
+    var testo: String
+    var voto: Int
+    var immagine: String
+
+    var body: some View 
+    {
+        HStack(spacing: 10)
+        {
+            Text(testo)
+            Text("\(voto)/30")
+            Image(systemName: immagine)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 10)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading).padding()
+    }
+}
+
 struct DettagliSimulazioneView: View {
+    var votoFinale: Int
     var body: some View {
         VStack
         {
             Titolo(titolo: "Simulazione")
-            Spacer()
+            if votoFinale >= 18
+            {
+                Intestazione(testo: "Promosso" , voto: votoFinale, immagine: "smile")
+            }
+            else
+            {
+                Intestazione(testo: "Bocciato" , voto: votoFinale, immagine: "smile")
+            }
         }
     }
 }
