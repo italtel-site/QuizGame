fa44027c02025a54c94725870d5324aa4a2607ba
Nadr <n.aboumoustafa@itsrizzoli.it>
2023-10-16 10:04:16 +0200

    fixata la tabbar - RICCARDO e NADR ha unito il login con la tabbar e unito le view

---
+++ QuizGame/VistaView.swift
@@ -0,0 +1,18 @@
+//
+//  VistaView.swift
+//  QuizGame
+//
+//  Created by user243551 on 10/13/23.
+//
+
+import SwiftUI
+
+struct VistaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaView()
+}
