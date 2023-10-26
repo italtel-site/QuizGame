d6346647a7f0080f93690ce52ffd9cf6ae31e14c
Nadr <n.aboumoustafa@itsrizzoli.it>
2023-10-26 22:04:49 +0200

    Abou - sistemato bug della non comparsa di un file in git e aggiunta una meccanica per stoppare quiz

---
+++ QuizGame/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  QuizGame
+//
+//  Created by user243551 on 10/26/23.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
